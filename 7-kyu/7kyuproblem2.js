ab89049f810a483ae7830ce9a8ec6b655426c3d0
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-03-31 22:48:00 -0400

    fixed typo

--- 7-kyu/7kyuproblem2.js
+++ 7-kyu/7kyuproblem2.js
@@ -1,6 +1,8 @@
-/* Your task is to make a function that can take any non-negative integer as an argument and 
+/* 
+Your task is to make a function that can take any non-negative integer as an argument and 
 return it with its digits in descending order. 
-Essentially, rearrange the digits to create the highest possible number. */
+Essentially, rearrange the digits to create the highest possible number. 
+*/
 
 function descendingOrder(n){
     let myStr = String(n)

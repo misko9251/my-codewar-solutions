641cab252c16187595884c95d54e83b48dba3acc
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-04-01 19:46:21 -0400

    Two to One

---
+++ 7-kyu/7kyuproblem4.js
@@ -0,0 +1,10 @@
+// Take 2 strings s1 and s2 including only letters from ato z. 
+// Return a new sorted string, the longest possible, containing distinct letters - 
+// each taken only once - coming from s1 or s2.
+
+function longest(s1, s2) {
+    let combined = s1.split('').concat(s2.split(''))
+    let doops = combined.filter((letter, index) => (combined.indexOf(letter) == index))
+    return doops.sort().join('')
+  }
+  longest("aretheyhere", "yestheyarehere")

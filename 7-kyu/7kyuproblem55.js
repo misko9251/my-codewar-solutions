786362c17a4acace43a21d21cdc93618b6ea689f
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-05-18 20:53:39 -0400

    count the number of JS developers coming from europe

---
+++ 7-kyu/7kyuproblem55.js
@@ -0,0 +1,9 @@
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+// Your task is to return the number of JavaScript developers coming from Europe.
+
+
+function countDevelopers(list) {
+    return list.filter((item)=>item.continent == 'Europe' && item.language == 'JavaScript').length
+  
+  }

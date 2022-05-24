44d650f3a426a21418f1d0d176befde5660f0842
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-05-23 20:05:20 -0400

    grouped by commas

---
+++ 6-kyu/6kyuproblem31.js
@@ -0,0 +1,6 @@
+// Finish the solution so that it takes an input n (integer) and returns a string that is the decimal representation of the number grouped by commas after every 3 digits.
+
+function groupByCommas(n) {
+    return n.toLocaleString()
+  }
+  groupByCommas(100000)

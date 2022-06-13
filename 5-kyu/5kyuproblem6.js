5ead88a798cfcad7616602546a4093b270f7561a
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-06-12 22:33:41 -0400

    first non repeating char

---
+++ 5-kyu/5kyuproblem6.js
@@ -0,0 +1,45 @@
+// DESCRIPTION:
+// Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+// As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+// If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+
+function firstNonRepeatingLetter(s) {
+    let lowerCaseWord = s.toLowerCase()
+    if(s == '') return ''
+    let arr = lowerCaseWord.split('')
+    let obj = {}
+    for(let letter of arr){
+      if(obj[letter]){
+        obj[letter]++
+      }else{
+        obj[letter] = 1
+      }
+    }
+    let duplicateLetters = {}
+    for(let duplicate of arr){
+      if(duplicateLetters[duplicate]){
+        duplicateLetters[duplicate]++
+      }else{
+        duplicateLetters[duplicate] = 1
+      }
+    }
+    let arrLetters = Object.entries(obj)
+    let ans = []
+    arrLetters.forEach((item)=>{
+      if(item[1] == 1){
+        ans.push(item[0])
+      }
+    })
+    if(s.length == 1){
+      return s
+    }else if(Object.entries(duplicateLetters).length == 1 || ans.length == 0){
+      return ''         
+    }else{
+      let indexNum = lowerCaseWord.indexOf(ans[0])
+      return s[indexNum]     
+    }
+  }

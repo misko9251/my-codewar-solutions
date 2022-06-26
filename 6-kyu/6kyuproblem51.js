9f5318b22488a63be51f63a87ea39293bb6d64f0
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-06-25 20:09:50 -0400

    longest palindrome

---
+++ 6-kyu/6kyuproblem51.js
@@ -0,0 +1,17 @@
+// Longest Palindrome
+// Find the length of the longest substring in the given string s that is the same in reverse.
+
+// As an example, if the input was “I like racecars that go fast”, the substring (racecar) length would be 7.
+
+// If the length of the input string is 0, the return value must be 0.
+
+
+longestPalindrome=function(s){
+    var l = 0;
+    for (var i = 0; i<s.length; i++)
+      for(var j = i; j<=s.length; j++)
+        if(s.substring(i,j) === s.substring(i,j).split('').reverse().join('') && s.substring(i,j).length > l)
+          l = s.substring(i,j).length;
+  
+    return l;
+  }

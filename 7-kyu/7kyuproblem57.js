bc6c67138a2cb7662e23dbb7c203c63333e784bb
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-06-30 21:37:56 -0400

    is ruby coming

---
+++ 7-kyu/7kyuproblem57.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return:
+
+// true if at least one Ruby developer has signed up; or
+// false if there will be no Ruby developers.
+
+function isRubyComing(list) {
+    let numOfRubyDevs = 0
+    list.forEach((dev)=>{
+      if(dev.language == 'Ruby'){
+        numOfRubyDevs++
+      }
+    })
+    return numOfRubyDevs >= 1 ? true : false
+  }

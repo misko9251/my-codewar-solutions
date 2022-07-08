4dbfa1c0a3ec8230956f138e4f82744c485431ca
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-07-07 21:34:35 -0400

    love v friendship

---
+++ 7-kyu/7kyuproblem64.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// If　a = 1, b = 2, c = 3 ... z = 26
+
+// Then l + o + v + e = 54
+
+// and f + r + i + e + n + d + s + h + i + p = 108
+
+// So friendship is twice stronger than love :-)
+
+// The input will always be in lowercase and never be empty.
+
+function wordsToMarks(string){
+    let alph = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let word = string.split('')
+    let total = 0
+    word.forEach((item,index)=>{
+      total += alph.indexOf(item)
+    })
+    return total
+  }

3651a23d2b14bf2440af964fbb9957eb89ce71ba
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-08-23 21:39:08 -0400

    highest scoring word

---
+++ 6-kyu/6kyuproblem63.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+function high(x){
+    let alph = ' abcdefghijklmnopqrstuvwxyz'
+    let scores = x.split(' ').map((item)=>item.split('').map((item)=>alph.indexOf(item))).map((item)=>item.reduce((a,b)=>a+b))
+    let highestIndex = scores.indexOf(Math.max(...scores))
+    return x.split(' ')[highestIndex]
+  
+  }
+  
+  high('man i need a taxi up to ubud')

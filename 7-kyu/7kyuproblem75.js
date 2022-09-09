21595d85248739921f461c7f69d2d8e64bf4640a
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-09-08 21:34:37 -0400

    filtr unused digits

---
+++ 7-kyu/7kyuproblem75.js
@@ -0,0 +1,14 @@
+// Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+function unusedDigits() {
+    let arr = [...arguments]
+    let numArr = arr.join('').split('').map((num)=>+num)
+    let answer = []
+    for(let i = 0; i <= 9; i++){
+      if(!numArr.includes(i)){
+        answer.push(i)
+      }
+    }
+    return answer.join('')
+  }
+  unusedDigits(2015, 8, 26)

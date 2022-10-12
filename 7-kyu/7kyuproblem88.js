7377d5ed3e4cfc0e988189b903b681575d44ee7a
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-10-11 21:34:28 -0400

    elevator distance

---
+++ 7-kyu/7kyuproblem88.js
@@ -0,0 +1,12 @@
+// Imagine you start on the 5th floor of a building, then travel down to the 2nd floor, then back up to the 8th floor. You have travelled a total of 3 + 6 = 9 floors of distance.
+
+// Given an array representing a series of floors you must reach by elevator, return an integer representing the total distance travelled for visiting each floor in the array in order.
+
+function elevatorDistance(array) {
+    let total = 0;
+    for(let i = 0; i < array.length-1; i++){
+      total += (Math.abs(array[i] - array[i+1]))
+    }
+    return total
+  }
+  elevatorDistance([5,2,8])
